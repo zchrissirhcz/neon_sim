f03c11519897b8dc7c8e5703bdf06b1deeb4d3c4
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-22 17:34:46 +0800

    fix ci; cleanup old tests; add first test

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -4,15 +4,13 @@ else()
   message(WARNING "ARTIFACTS_DIR env var not defined, abort")
 endif()
 
-#--- GoogleTest
+# googletest
 if(ANDROID)
   set(GTest_DIR "${ARTIFACTS_DIR}/googletest/1.11.0/android-arm64/lib/cmake/GTest" CACHE PATH "Directory that contains GTestConfig.cmake")
 elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
   set(GTest_DIR "${ARTIFACTS_DIR}/googletest/1.11.0/linux-x64/lib/cmake/GTest" CACHE PATH "Directory that contains GTestConfig.cmake")
 elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
-  set(GTest_DIR "${ARTIFACTS_DIR}/googletest/1.11.0/windows-x64/lib/cmake/GTest" CACHE PATH "Directory that contains GTestConfig.cmake")
-elseif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-  set(GTest_DIR "${ARTIFACTS_DIR}/googletest/1.11.0/mac-x64/lib/cmake/GTest" CACHE PATH "Directory that contains GTestConfig.cmake")
+  message(WARNING "GTest_DIR not set yet")
 else()
   message(WARNING "GTest_DIR not set yet")
 endif()
@@ -20,57 +18,17 @@ message(STATUS ">>> GTest_DIR is: ${GTest_DIR}")
 find_package(GTest REQUIRED)
 
 
-#--- libpng
+# opencv
 if(ANDROID)
-  #set(png_DIR "${ARTIFACTS_DIR}/libpng/1.6.38-dev/linux-x64/lib/libpng")
-  # set(png_lib "${ARTIFACTS_DIR}/libpng/1.6.38-dev/android-arm64/lib/libpng.a")
-  # set(png_include_dir "${ARTIFACTS_DIR}/libpng/1.6.38-dev/android-arm64/include")
-  set(libpng_install_dir "/home/zz/artifacts/libpng/1.6.38-dev/android-arm64")
+  set(OpenCV_DIR "${ARTIFACTS_DIR}/opencv/android-arm64/4.5.4/sdk/native/jni" CACHE PATH "")
 elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
-  # set(png_lib "${ARTIFACTS_DIR}/libpng/1.6.38-dev/linux-x64/lib/libpng16.a")
-  # set(png_include_dir "${ARTIFACTS_DIR}/libpng/1.6.38-dev/linux-x64/include")
-  set(libpng_install_dir "/home/zz/artifacts/libpng/1.6.38-dev/linux-x64")
+  set(OpenCV_DIR "${ARTIFACTS_DIR}/opencv/linux-x64/4.5.5/lib/cmake/opencv4" CACHE PATH "")
+elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
+  message(WARNING "OpenCV_DIR not set yet")
+else()
+  message(WARNING "OpenCV_DIR not set yet")
 endif()
-
-#include("${libpng_install_dir}/lib/libpng/libpng16.cmake")
-# set_target_properties(png
-#   PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${libpng_install_dir}/include/libpng16
-# )
-# set_target_properties(png_static
-#   PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${libpng_install_dir}/include/libpng16
-# )
-
-#find_package(libpng16 REQUIRED)
-
-
-#--- OpenCV
-set(A_VERSION "4.5.5")
-if(CMAKE_SYSTEM_NAME MATCHES "Windows")
-  set(OpenCV_DIR "${ARTIFACTS_DIR}/opencv/windows/${A_VERSION}")
-elseif(ANDROID)
-  set(OpenCV_DIR "${ARTIFACTS_DIR}/opencv/android-arm64/${A_VERSION}/sdk/native/jni")
-elseif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-  message("not supported platform(${CVPKG_TARGET_PLATFORM}) for package OpenCV")
-elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
-  set(OpenCV_DIR "${ARTIFACTS_DIR}/opencv/linux-x64/${A_VERSION}/lib/cmake/opencv4")
-endif()
-
-message(STATUS ">>> OpenCV_DIR: ${OpenCV_DIR}")
+message(STATUS ">>> OpenCV_DIR is: ${OpenCV_DIR}")
 find_package(OpenCV REQUIRED)
 
 
-
-#--- ncnn
-if(CMAKE_SYSTEM_NAME MATCHES "Windows")
-  set(ncnn_DIR "D:/dev/ncnn/build/vs2019-x64/install/lib/cmake/ncnn")
-elseif(ANDROID)
-  #set(ncnn_DIR "D:/dev/ncnn/build/android-arm64/install/lib/cmake/ncnn")
-  #set(ncnn_DIR "${ARTIFACTS_DIR}/ncnn/20211208/android-armv8/lib/cmake/ncnn")
-  set(ncnn_DIR "${ARTIFACTS_DIR}/ncnn/20220216/android-arm64/lib/cmake/ncnn")
-elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
-  set(ncnn_DIR "${ARTIFACTS_DIR}/ncnn/20220216/linux-x64/lib/cmake/ncnn")
-else()
-  message(WARNING "ncnn_DIR not set yet")
-endif()
-message(STATUS ">>> ncnn_DIR: ${ncnn_DIR}")
-#find_package(ncnn REQUIRED)
---
+++ tests/test_vext.cpp
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+
+#define NEON_SIM_IMPLEMENTATION
+
+#if __ARM_NEON
+#include <arm_neon.h>
+#include "arm_neon_helper.hpp"
+#else
+#include "arm_neon_sim.hpp"
+#endif // __ARM_NEON
+
+
+bool almostEqual(const uint8x8_t& expected, const uint8x8_t& actual, double eps=0)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        const double diff = fabs((double)expected[i] - (double)actual[i]);
+        if (diff > eps)
+        {
+            std::cerr << "array[" << i << "] (" << (int)actual[i] << ") != expected[" << i << "] (" << (int)expected[i] 
+                      << "), diff = " << diff << ", EPS = " << eps << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+TEST(vext, u8)
+{
+    printf("Hello\n");
+    uint8x8_t src1 = {1, 2, 3, 4, 5, 6, 7, 8};
+    uint8x8_t src2 = {9, 10, 11, 12, 13, 14, 15, 16};
+    uint8x8_t expected = {4, 5, 6, 7, 8, 9, 10, 11};
+    uint8x8_t actual = vext_u8(src1, src2, 3);
+    EXPECT_TRUE(almostEqual(expected, actual));
+}
