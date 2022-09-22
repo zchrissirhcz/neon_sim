c58bde8bc8d928745aa3f943438df53640fe846a
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-22 19:40:38 +0800

    fix linux-aarch64 build

--- tests/test_vext.cpp
+++ tests/test_vext.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 
+#include <cmath>
+
 #define NEON_SIM_IMPLEMENTATION
 
 #if __ARM_NEON
@@ -14,7 +16,7 @@ bool almostEqual(const uint8x8_t& expected, const uint8x8_t& actual, double eps=
 {
     for (int i = 0; i < 8; i++)
     {
-        const double diff = fabs((double)expected[i] - (double)actual[i]);
+        const double diff = std::fabs((double)expected[i] - (double)actual[i]);
         if (diff > eps)
         {
             std::cerr << "array[" << i << "] (" << (int)actual[i] << ") != expected[" << i << "] (" << (int)expected[i] 
