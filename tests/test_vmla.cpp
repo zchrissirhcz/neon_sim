5bb313bf76edf35c7f7e4918662a9fa899675f58
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-22 21:20:30 +0800

    more unittest for vmla and vmul

--- tests/test_vmla.cpp
+++ tests/test_vmla.cpp
@@ -9,3 +9,14 @@ TEST(vmlaq, f32)
     float32x4_t expected = { 5.0, 7.0, 9.0, 11.0 };
     EXPECT_TRUE(almostEqual(expected, actual));
 }
+
+
+TEST(vmlaq_n, f32)
+{
+    float32x4_t v1 = { 1.0, 2.0, 3.0, 4.0 };
+    float32x4_t v2 = { 1.0, 1.0, 1.0, 1.0 };
+    float32_t s = 3.0;
+    float32x4_t actual = vmlaq_n_f32(v1, v2, s);
+    float32x4_t expected = { 4.0, 5.0, 6.0, 7.0 };
+    EXPECT_TRUE(almostEqual(expected, actual));
+}
--- tests/test_vmul.cpp
+++ tests/test_vmul.cpp
@@ -8,3 +8,12 @@ TEST(vmulq, f32)
     float32x4_t expected = { 1.0, 2.0, 3.0, 4.0 };
     EXPECT_TRUE(almostEqual(expected, actual));
 }
+
+TEST(vmulq_n, f32)
+{
+    float32x4_t v = { 1.0, 2.0, 3.0, 4.0 };
+    float32_t s = 3.0;
+    float32x4_t actual = vmulq_n_f32(v, s);
+    float32x4_t expected = { 3.0, 6.0, 9.0, 12.0 };
+    EXPECT_TRUE(almostEqual(expected, actual));
+}
