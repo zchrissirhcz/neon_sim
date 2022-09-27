424efd6775e24a5a012d62c4bee3540838de60ed
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-27 22:46:48 +0800

    cleanup: unittest to proper files

---
+++ tests/test_vhsub.cpp
@@ -0,0 +1,10 @@
+#include "test_util.hpp"
+
+TEST(sub, vhsub)
+{
+    int8x8_t v1 = vdup_n_s8(120);
+    int8x8_t v2 = {1, 2, 3, 4, 120, 121, 122, 123};
+    int8x8_t actual = vhsub_s8(v1, v2);
+    int8x8_t expected = {59, 59, 58, 58, 0, -1, -1, -2};
+    EXPECT_TRUE(almostEqual(expected, actual));
+}
---
+++ tests/test_vrsubhn.cpp
@@ -0,0 +1,10 @@
+#include "test_util.hpp"
+
+TEST(sub, vrsubhn)
+{
+    int16x8_t v1 = vdupq_n_s16(850);
+    int16x8_t v2 = {200, 400, 600, 800, 1000, 1200, 1400, 1600};
+    int8x8_t actual = vrsubhn_s16(v1, v2);
+    int8x8_t expected = {3, 2, 1, 0, -1, -1, -2, -3};
+    EXPECT_TRUE(almostEqual(expected, actual));
+}
