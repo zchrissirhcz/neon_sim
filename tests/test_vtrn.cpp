e0e399ad06d638d681cb049d0f34bc1580156553
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-29 20:51:28 +0800

    try

--- tests/test_vtrn.cpp
+++ tests/test_vtrn.cpp
@@ -3,8 +3,8 @@
 TEST(vtrn, u8)
 {
     uint8x8x2_t src;
-    src.val[0] = {1, 2, 3, 4, 5, 6, 7, 8};
-    src.val[1] = {9, 10, 11, 12, 13, 14, 15, 16};
+    src.val[0] = uint8x8_t{1, 2, 3, 4, 5, 6, 7, 8};
+    src.val[1] = uint8x8_t{9, 10, 11, 12, 13, 14, 15, 16};
     uint8x8x2_t actual = vtrn_u8(src.val[0], src.val[1]);
     uint8x8x2_t expected;
     expected.val[0] = {1, 9, 3, 11, 5, 13, 7, 15};
