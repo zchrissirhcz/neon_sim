2b5d631733a2622c3a60fb24691fe47b1e71faa1
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-29 20:57:35 +0800

    Fix

--- tests/test_vuzp.cpp
+++ tests/test_vuzp.cpp
@@ -3,12 +3,12 @@
 TEST(vuzp, u8)
 {
     uint8x8x2_t src;
-    src.val[0] = {1, 2, 3, 4, 5, 6, 7, 8};
-    src.val[1] = {9, 10, 11, 12, 13, 14, 15, 16};
+    src.val[0] = uint8x8_t{1, 2, 3, 4, 5, 6, 7, 8};
+    src.val[1] = uint8x8_t{9, 10, 11, 12, 13, 14, 15, 16};
     uint8x8x2_t actual = vuzp_u8(src.val[0], src.val[1]);
     uint8x8x2_t expected;
-    expected.val[0] = {1, 3, 5, 7, 9, 11, 13, 15};
-    expected.val[1] = {2, 4, 6, 8, 10, 12, 14, 16};
+    expected.val[0] = uint8x8_t{1, 3, 5, 7, 9, 11, 13, 15};
+    expected.val[1] = uint8x8_t{2, 4, 6, 8, 10, 12, 14, 16};
 
     EXPECT_TRUE(almostEqual(expected, actual));
 }
