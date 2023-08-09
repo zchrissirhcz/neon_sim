1884af46de71e402e74d551af8b9d4f33d1d9111
Zhuo Zhang <imzhuo@foxmail.com>
2023-08-10 05:11:14 +0800

    [clean] Replace old sleek.cmake with precise asan.cmake and debug_symbols.cmake

--- cmake/asan.cmake
+++ cmake/asan.cmake
@@ -1,48 +1,71 @@
-# last update: 2022/05/02
+# Author: Zhuo Zhang <imzhuo@foxmail.com>
+# Homepage: https://github.com/zchrissirhcz
+# Last update: 2023-08-04 23:55:28
 
-option(USE_ASAN "Use Address Sanitizer?" ON)
-#--------------------------------------------------
-# globally setting
-#--------------------------------------------------
+option(VS2022_ASAN_DISABLE_VECTOR_ANNOTATION "Disable string annotation for VS2022 ASan?" ON)
+option(VS2022_ASAN_DISABLE_STRING_ANNOTATION "Disable vector annotation for VS2022 ASan?" ON)
+
+# globally
 # https://stackoverflow.com/a/65019152/2999096
 # https://docs.microsoft.com/en-us/cpp/build/cmake-presets-vs?view=msvc-170#enable-addresssanitizer-for-windows-and-linux
-if(USE_ASAN)
-  if((CMAKE_C_COMPILER_ID STREQUAL "MSVC") OR (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC"))
-    set(ASAN_FLAGS "/fsanitize=address")
-  elseif(MSVC AND ((CMAKE_C_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")))
-    message(FATAL_ERROR "Clang-CL not support setup AddressSanitizer via CMakeLists.txt")
-  elseif((CMAKE_C_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-      OR (CMAKE_C_COMPILER_ID MATCHES "Clang") OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
-    # Note: `-g` is explicitly required here for debug symbol information such line number and file name
-    # There is NDK's android.toolchain.cmake's `-g`, in case user removed it, explicitly add `-g` here.
-    set(ASAN_FLAGS "-g -fno-omit-frame-pointer -fsanitize=address")
-  endif()
-  
-  message(STATUS ">>> USE_ASAN: YES")
-  message(STATUS ">>> CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
-  if(CMAKE_BUILD_TYPE MATCHES "Debug")
-    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${ASAN_FLAGS}")
-    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${ASAN_FLAGS}")
-    set(CMAKE_LINKER_FLAGS_DEBUG "${CMAKE_LINKER_FLAGS_DEBUG} ${ASAN_FLAGS}")
-  elseif(CMAKE_BUILD_TYPE MATCHES "Release")
-    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} ${ASAN_FLAGS}")
-    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${ASAN_FLAGS}")
-    set(CMAKE_LINKER_FLAGS_RELEASE "${CMAKE_LINKER_FLAGS_RELEASE} ${ASAN_FLAGS}")
-  elseif(CMAKE_BUILD_TYPE MATCHES "RelWithDebInfo")
-    set(CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO} ${ASAN_FLAGS}")
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} ${ASAN_FLAGS}")
-    set(CMAKE_LINKER_FLAGS_RELWITHDEBINFO "${CMAKE_LINKER_FLAGS_RELWITHDEBINFO} ${ASAN_FLAGS}")
-  elseif(CMAKE_BUILD_TYPE MATCHES "MinSizeRel")
-    set(CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS_MINSIZEREL} ${ASAN_FLAGS}")
-    set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_MINSIZEREL} ${ASAN_FLAGS}")
-    set(CMAKE_LINKER_FLAGS_MINSIZEREL "${CMAKE_LINKER_FLAGS_MINSIZEREL} ${ASAN_FLAGS}")
-  elseif(CMAKE_BUILD_TYPE EQUAL "None" OR NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ASAN_FLAGS}")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ASAN_FLAGS}")
-    set(CMAKE_LINKER_FLAGS "${CMAKE_LINKER_FLAGS} ${ASAN_FLAGS}")
+set(ASAN_AVAILABLE ON)
+if((CMAKE_C_COMPILER_ID STREQUAL "MSVC") OR (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC"))
+  if((CMAKE_C_COMPILER_VERSION STRLESS 16.0) OR (CMAKE_CXX_COMPILER_VERSION STRLESS 16.0))
+    message(WARNING "ASAN is available since VS2019, please use higher version of VS")
+    set(ASAN_AVAILABLE OFF)
+  elseif( ((CMAKE_C_COMPILER_VERSION STRGREATER_EQUAL 16.0) AND (CMAKE_C_COMPILER_VERSION STRLESS 16.7))
+    OR ((CMAKE_CXX_COMPILER_VERSION STRGREATER_EQUAL 16.0) AND (CMAKE_CXX_COMPILER_VERSION STRLESS 16.7)) )
+    # https://devblogs.microsoft.com/cppblog/asan-for-windows-x64-and-debug-build-support/
+    message(WARNING "VS2019 x64 ASAN requires VS >= 16.7, please update VS")
+    set(ASAN_AVAILABLE OFF)
   else()
-    message(FATAL_ERROR "Unsupported CMAKE_BUILD_TYPE for asan setup: ${CMAKE_BUILD_TYPE}")
+    set(ASAN_OPTIONS /fsanitize=address)
+  endif()
+elseif(MSVC AND ((CMAKE_C_COMPILER_ID STREQUAL "Clang") OR (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")))
+  message(WARNING "Clang-CL not support setup AddressSanitizer via CMakeLists.txt")
+  set(ASAN_AVAILABLE OFF)
+elseif((CMAKE_C_COMPILER_ID MATCHES "GNU") OR (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  OR (CMAKE_C_COMPILER_ID MATCHES "Clang") OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
+  set(ASAN_OPTIONS -fsanitize=address -fno-omit-frame-pointer -g)
+endif()
+
+if(ASAN_AVAILABLE)
+  message(STATUS ">>> USE_ASAN: YES")
+  add_compile_options(${ASAN_OPTIONS})
+  if((CMAKE_SYSTEM_NAME MATCHES "Windows") AND ((CMAKE_C_COMPILER_ID STREQUAL "MSVC") OR (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")))
+    add_link_options(/ignore:4300) # /INCREMENTAL
+    add_link_options(/DEBUG) # LNK4302
+
+    if(CMAKE_CXX_COMPILER_VERSION STRGREATER_EQUAL 17.2)
+      if(VS2022_ASAN_DISABLE_VECTOR_ANNOTATION)
+        # https://learn.microsoft.com/en-us/cpp/sanitizers/error-container-overflow?view=msvc-170
+        add_definitions(-D_DISABLE_VECTOR_ANNOTATION)
+        message(STATUS ">>> VS2022_ASAN_DISABLE_VECTOR_ANNOTATION: YES")
+      else()
+        message(STATUS ">>> VS2022_ASAN_DISABLE_VECTOR_ANNOTATION: NO")
+      endif()
+    endif()
+
+    if(CMAKE_CXX_COMPILER_VERSION STRGREATER_EQUAL 17.6)
+      if(VS2022_ASAN_DISABLE_STRING_ANNOTATION)
+        # https://learn.microsoft.com/en-us/cpp/sanitizers/error-container-overflow?view=msvc-170
+        add_definitions(-D_DISABLE_STRING_ANNOTATION)
+        message(STATUS ">>> VS2022_ASAN_DISABLE_STRING_ANNOTATION: YES")
+      else()
+        message(STATUS ">>> VS2022_ASAN_DISABLE_STRING_ANNOTATION: NO")
+      endif()
+    endif()
+
+  else()
+    add_link_options(${ASAN_OPTIONS})
   endif()
 else()
   message(STATUS ">>> USE_ASAN: NO")
 endif()
+
+
+# per-target
+# https://developer.android.com/ndk/guides/asan?hl=zh-cn#cmake
+# target_compile_options(${TARGET} PUBLIC -fsanitize=address -fno-omit-frame-pointer)
+# set_target_properties(${TARGET} PROPERTIES LINK_FLAGS -fsanitize=address) # not working for INTERFACE targets
+# target_link_options(${TARGET} INTERFACE -fsanitize=address) # for INTERFACE targets
